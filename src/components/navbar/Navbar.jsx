4112bfa55f6edde3527a5541f7dab863f9de0baf
Op-Pautu <pautuop@gmail.com>
2023-09-06 14:02:42 +0530

    Add styles to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,29 @@
+import Image from 'next/image'
 import styles from './navbar.module.css'
+import Link from 'next/link'
+import AuthLinks from '../authLinks/AuthLinks'
+import ThemeToggle from '../themeToggle/ThemeToggle'
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src='/facebook.png' alt='facebook' width={24} height={24}/>
+        <Image src='/instagram.png' alt='instagram' width={24} height={24}/>
+        <Image src='/tiktok.png' alt='tiktok' width={24} height={24}/>
+        <Image src='/youtube.png' alt='youtube' width={24} height={24}/>
+      </div>
+      <div className={styles.logo}>OP&apos;s Blog</div>
+      <div className={styles.links}>
+        <ThemeToggle/>
+        <Link href="/">Home</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/about">About</Link>
+        <AuthLinks/>
+      
+      </div>
+      
+      
+    </div>
   )
 }
 
