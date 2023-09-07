49b8e0d1f073034362686d0d3ff5a5411d3e8da3
Op-Pautu <pautuop@gmail.com>
2023-09-07 16:28:18 +0530

    Add useContext to implement dark/light mode

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,23 @@
+"use client";
+ 
+import Image from 'next/image'
 import styles from './themeToggle.module.css'
+import { useContext } from 'react'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToggle = () => {
+  const {theme, toggle} = useContext(ThemeContext)
+  console.log(theme);
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div 
+      className={styles.container} 
+      onClick={toggle}
+      style={theme === "dark" ? { backgroundColor: "white"} : {backgroundColor: '#0f172a'}}
+    >
+      <Image src='/moon.png' width={14} height={14} alt='themeIcon'/>
+      <div className={styles.ball} style={theme === "dark" ? {left: 1, backgroundColor: "#0f172a"} : {right: 1, backgroundColor: 'white'}}></div>
+      <Image src='/sun.png' width={14} height={14} alt='themeIcon'/>
+    </div>
   )
 }
 
