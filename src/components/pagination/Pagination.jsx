e7e69f3186e8d05b3b8bb46ac9211d34574c7bb5
Op-Pautu <pautuop@gmail.com>
2023-09-08 12:57:26 +0530

    Style pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,25 @@
-import styles from './pagination.module.css'
-const Pagination = () => {
-  return (
-    <div className={styles.container}>Pagination</div>
-  )
-}
+"use client";
 
-export default Pagination
+import React from "react";
+import styles from "./pagination.module.css";
+
+
+const Pagination = () => {
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
