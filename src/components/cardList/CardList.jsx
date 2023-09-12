444c1ea71853f0fb44ae885c80713650526f2e59
Op-Pautu <pautuop@gmail.com>
2023-09-12 17:16:45 +0530

    Attempt to fix problem

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,12 +3,9 @@ import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css";
 
 const getData = async (page, cat) => {
-  const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
-    }
-  );
+  const res = await fetch(`/api/posts?page=${page}&cat=${cat || ""}`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed");
